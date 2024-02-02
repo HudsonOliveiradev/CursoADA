921c4e8fe4ecf1fa6076b6046e8263d8539e4620
Hudson Alves de Oliveira <hudsonbiomed15@gmail.com>
2024-02-02 19:39:57 -0300

    Projeto estruturado, faltando ver o Salvar.

--- src/AgendaTelefonicaClasse.java
+++ src/AgendaTelefonicaClasse.java
@@ -1,11 +1,10 @@
 import java.io.*;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Scanner;
 
-public class agendaTelefonica {
+public class AgendaTelefonicaClasse {
     private static final String Arquivo_Contato = "contatos.txt";
     private static Scanner scanner = new Scanner(System.in);
 
@@ -25,7 +24,8 @@ public class agendaTelefonica {
                  editarContato();
                  break;
              case 4:
-                 sair();
+                 System.out.println("O sistema será encerrado!");
+                 emExecucao = sair();
                  break;
              default:
                  System.out.println("Opção Inválida");
@@ -50,14 +50,17 @@ public class agendaTelefonica {
             System.out.println("Digite DDD");
             String ddd = scanner.nextLine();
             System.out.println("Digite o número de telefone");
-            Long numero = scanner.nextLong();
+            String numero = scanner.nextLine();
             List<Telefone>telefones = new ArrayList<>();
-            Telefone contatoNum = new Telefone(ddd, numero);
+            Telefone contatoNum = new Telefone(ddd, Long.parseLong(String.valueOf(numero)));
             telefones.add(contatoNum);
             Contato nomeCadastrado = new Contato(nome, sobrenome, telefones);
 
             bWcompleto.write(CriarContato(nomeCadastrado));
             bWcompleto.newLine();
+
+            escrever.close();
+            bWcompleto.close();
 
             System.out.println("Cadastro Finalizado!");
         } catch (IOException e) {
@@ -71,10 +74,13 @@ public class agendaTelefonica {
         contato.setId(id);
         novoContato.concat(contato.getId().toString() + " | ");
         novoContato.concat(contato.getNome() + " " + contato.getSobreNome());
-        novoContato.concat(" | " + contato.getTelefones());
+        novoContato.concat(" | " + contato.getTelefones().get(0).getDdd());
+        novoContato.concat(" | " + contato.getTelefones().get(0).getNumero());
         return novoContato;
     }
-    public static void RemoverContato(Long id){
+    public static void removerContato(){
+        System.out.println("Qual Id que você deseja remover?");
+        Long id = scanner.nextLong();
         File arquivo = new File(Arquivo_Contato);
         try {
             FileReader fileReader = new FileReader(arquivo);
@@ -102,6 +108,74 @@ public class agendaTelefonica {
         } catch (IOException e) {
             System.err.println("Erro ao remover contato!" + e.getMessage());
         }
+    }
 
+    public static void editarContato(){
+        System.out.println("Qual Id que você deseja editar?");
+        Long idEditado = scanner.nextLong();
+
+        File arquivo = new File(Arquivo_Contato);
+        System.out.println("Você deseja editar: nome, ddd ou telefone?");
+        String valorEditado = scanner.nextLine();
+        int posicaoEditado = 0;
+        switch (valorEditado) {
+            case "nome":
+                posicaoEditado = 2;
+                break;
+            case "sobrenome":
+                posicaoEditado = 3;
+                break;
+            case "ddd":
+                posicaoEditado = 5;
+                break;
+            case "telefone":
+                posicaoEditado = 7;
+                break;
+            default:
+                System.out.println("O valor informado deve estar igual ao impresso!");
+        }
+
+        try {
+            FileReader fileReader = new FileReader(arquivo);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String linha;
+
+            StringBuilder textoVazio = new StringBuilder();
+            while ((linha = bufferedReader.readLine()) != null){
+                if(linha.contains(idEditado.toString())){
+                    String[] split = linha.split(" ");
+
+                    String linhaEditada = "";
+
+                    for (int i = 0; i < split.length; i++){
+                        if(i == posicaoEditado){
+                            linhaEditada.concat(valorEditado + " ");
+                        } else {
+                            linhaEditada.concat(split[i] + " ");
+                        }
+                    }
+
+                    textoVazio.append(linhaEditada).append(System.lineSeparator());
+                }
+            }
+            bufferedReader.close();
+            fileReader.close();
+
+            FileWriter fileWriter = new FileWriter(arquivo);
+            BufferedWriter bufferedWriter= new BufferedWriter(fileWriter);
+
+            bufferedWriter.write(textoVazio.toString());
+            bufferedWriter.close();
+            fileWriter.close();
+
+            System.out.println("Contato editado com sucesso!");
+
+        } catch (IOException e) {
+            System.err.println("Erro ao editar contato!" + e.getMessage());
+        }
+
+    }
+    public static boolean sair(){
+        return false;
     }
 }
